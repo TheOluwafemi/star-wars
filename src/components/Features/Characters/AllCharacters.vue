520dfc5b3a9eaa41850a325bb0bb939a39a654b7
theOluwafemi <oluwafemidamilola21@gmail.com>
2019-08-13 02:20:42 +0100

    all characters from API, pagination added

--- src/components/Features/Characters/AllCharacters.vue
+++ src/components/Features/Characters/AllCharacters.vue
@@ -1,33 +1,131 @@
 <template>
-  <div class="container">
-    <h1 class="text-center">Popular Characters</h1>
-    <div class="row">
-      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" v-for="(character, index) in characters" v-if="index <= 3" :key="character.name">
-          <div class="uk-card uk-card-default uk-child-width-1-2@s" uk-grid>
-              <div class="uk-card-media-left uk-cover-container float-left">
-                  <img src="../../../assets/character-1.jpg" alt="" uk-cover>
-                  <!-- <canvas width="600" height="400"></canvas> -->
+  <div>
+    <Header @search:webpage="searchWebpage" />
+    <div class="container all-starships">
+      <h2 class="heading">All Characters</h2>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"  v-for="(character, index) in displayedCharacters" :key="index">
+          <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+              <img src="../../../assets/character-3.jpg" alt="ship image">
+            </div>
+            <div class="uk-card-body">
+              <h4><a class="header">{{character.name}}</a></h4>
+              <div class="description">
+                <p>Gender: {{character.gender}}</p>
+                <p>Birth Year: {{character.birth_year}}</p>
               </div>
-              <div class="uk-card-body float-right">
-                  <h3 class="uk-card-title">Media Left</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-              </div>
+            </div>
+            <div class="uk-card-footer">
+              <h6><a class="float-right">
+                READ MORE
+              </a></h6>
+            </div>
           </div>
+        </div>
+      </div>
+
+      <div class="pagination-section">
+        <ul class="uk-pagination uk-flex-right uk-text-bold">
+          <li v-for="(n, index) in numOfPages" :key="index">
+            <a href="" @click.prevent="setPage(n)">{{n}}</a>
+          </li>
+        </ul>
+
       </div>
     </div>
-    <div class="text-center">
-      <button class="ui button">View More</button>
-    </div>
   </div>
+
+  
 </template>
 
 <script>
-export default {
-  name: 'PopularCharacters',
+import Header from '../../core/Header'
 
-  props: {
-    characters: Array,
+export default {
+  name: 'AllCharacters',
+
+  components: {
+    Header
   },
+
+  data() {
+    return {
+      characters: [],
+      pageNumber: 0,
+      currentPage: 1,
+      perPage: 6,
+      perPageOptions: [6,9],
+    }
+  },  
+
+  methods: {
+
+    async fetchAllCharacters() {
+      try {
+        let morePagesAvailable = true;
+        let currentPage = 0;
+
+        while(morePagesAvailable) {
+          currentPage++;
+          const response = await fetch(`https://swapi.co/api/people/?page=${currentPage}`)
+          let { results, count } = await response.json();
+          results.forEach(e => this.characters.unshift(e));
+          morePagesAvailable = currentPage < 4;
+        }
+        return this.characters;
+      } catch (error) {
+          console.error(error)
+      }
+    },
+
+    getPic(index) {
+      return './images/' + this.pics[index] + '.jpg';
+    },
+
+    getImagePath() {
+      let i;
+      pics: ['starship-1', 'starship-2', 'starship-3', 'starship-4', 'starship-5', 'starship-6'];
+      for (i = 0; i < pics.length; i++) {
+        return './images/' + pics[i] + '.jpg';
+      }
+    },
+
+    searchWebpage(search) {
+      console.log('search', search);
+      this.characters = this.characters.filter(characters => characters.name.toLowerCase().includes(search.toLowerCase()))
+    },
+
+    setPage(n) {
+    	this.currentPage = n;
+    },
+  },
+
+  computed:{
+  	offset() {
+    	return ((this.currentPage - 1) * this.perPage);
+    },
+
+    limit() {
+    	return (this.offset + this.perPage);
+    },
+
+    numOfPages() {
+    	return Math.ceil(this.characters.length / this.perPage);
+    },
+
+    displayedCharacters() {
+      if (this.offset > this.characters.length) {
+      	this.currentPage = this.numOfPages;
+      }
+    	return this.characters.slice(this.offset, this.limit);
+    }
+
+  },
+
+  beforeMount() {
+    this.fetchAllCharacters();
+  }
 
 
 }
@@ -50,7 +148,32 @@ a {
   color: #42b983;
 }
 
+.all-starships {
+  margin-top: 1.5em;
+  margin-bottom: 1.5em;
+}
+
+.heading {
+  text-align: center;
+  position: relative;
+  margin-top: 1.5em;
+  margin-bottom: 2em;
+}
+.heading::after {
+    content: '';
+    height: 5px;
+    width: 5%;
+    background: #5b5150;
+    position: absolute;
+    left: calc(57% - 10%);
+    bottom: -12px;
+}
+
 div .uk-card {
   margin-bottom: 2em !important;
+}
+
+.pagination-section {
+  float: right;
 }
 </style>
